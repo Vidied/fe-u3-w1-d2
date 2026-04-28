30e307ae6193dbfbe4821ea024ac838100854be0
Vidied <davidepan2001@gmail.com>
2026-04-28 16:56:50 +0200

    compito finito

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-
-import './App.css'
-
-import MyNav from './components/MyNav'
-import MyFooter from './components/MyFooter'
-import Welcome from './components/Welcome'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
+import AllTheBooks from "./components/AllTheBooks";
+import { Container } from "react-bootstrap";
 
 function App() {
-
-
-  return (<div>
-<header>
-  <MyNav></MyNav>
-</header>
-<main><Welcome></Welcome></main>
-<footer><MyFooter>
-  </MyFooter></footer></div>
-  )
+  return (
+    <div>
+      <header>
+        <MyNav></MyNav>
+      </header>
+      <main>
+        <Container>
+          <Welcome></Welcome>
+          <AllTheBooks></AllTheBooks>
+        </Container>
+      </main>
+      <footer>
+        <MyFooter></MyFooter>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,36 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import libriFantasy from "../assets/Libri/fantasy.json";
+import { Container, Row, Col } from "react-bootstrap";
+
+function AllTheBooks() {
+  return (
+    <Container>
+      <Row className="g-2">
+        {libriFantasy.map((libri) => {
+          return (
+            <Col xs={12} md={6} lg={3} key={libri.asin}>
+              <Card className="h-100">
+                <Card.Img
+                  style={{ maxWidth: 150 }}
+                  variant="top"
+                  src={libri.img}
+                />
+                <Card.Body className="d-flex flex-column justify-content-between">
+                  <Card.Title>{libri.title}</Card.Title>
+                  <Card.Text>
+                    <div>Categoria:{libri.category}</div>
+                    <div>Prezzo:{libri.price}</div>
+                  </Card.Text>
+                  <Button variant="primary">Compra</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
+
+export default AllTheBooks;
